66dc3b93cde85f2bf2012d8f10165b0b6595e09d
Chris Fallin <cfallin@mozilla.com>
2019-12-16 15:30:28 -0800

    WIP.

--- cranelift-codegen/src/machinst/pattern.rs
+++ cranelift-codegen/src/machinst/pattern.rs
@@ -32,8 +32,47 @@ use crate::HashMap;
  *         and need not be generated.
  */
 
-pub struct LowerTable {
+/// A table of tree patterns and lowering actions.
+pub struct LowerTable<Op: MachInstOp, Arg: MachInstArg> {
     prefix_pool: PatternPrefixPool,
+    entries: HashMap<Opcode, SmallVec<[LowerTableEntry<Op, Arg>; 4]>>,
+}
+
+/// A single entry in the lowering table.
+pub struct LowerTableEntry<Op: MachInstOp, Arg: MachInstArg> {
+    /// The prefix (tree pattern) associated with this entry.
+    pub prefix: PatternPrefixRange,
+    /// The lowering action to perform.
+    pub action: LowerAction<Op, Arg>,
+}
+
+impl<Op: MachInstOp, Arg: MachInstArg> LowerTable<Op, Arg> {
+    /// Create a new lowering table.
+    pub fn new() -> LowerTable<Op, Arg> {
+        LowerTable {
+            prefix_pool: PatternPrefixPool::new(),
+            entries: HashMap::new(),
+        }
+    }
+
+    /// Get the prefix pool of the table to allow building of a new prefix.
+    pub fn pool<'a>(&'a mut self) -> &'a mut PatternPrefixPool {
+        &mut self.prefix_pool
+    }
+
+    /// Add a pattern to the lowering table.
+    pub fn add(&mut self, prefix: PatternPrefixRange, action: LowerAction<Op, Arg>) {
+        let op = self.prefix_pool.get(&prefix).root_op();
+        self.entries
+            .entry(op)
+            .or_insert_with(SmallVec::new)
+            .push(LowerTableEntry { prefix, action });
+    }
+
+    /// Get the entries associated with a given root opcode.
+    pub fn get_entries(&self, root_op: Opcode) -> Option<&[LowerTableEntry<Op, Arg>]> {
+        self.entries.get(&root_op).map(|v| &v[..])
+    }
 }
 
 /// An action to perform when matching a tree of ops. Returns `true` if codegen was successful.
